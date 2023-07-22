5b649511c58c79700bf1af7e0f16916c962d8b29
kraftonius <avd@yandex.ru>
2023-07-22 12:38:45 +0300

    upgrade Homework

--- course/Seminar8/telephone/controller.py
+++ course/Seminar8/telephone/controller.py
@@ -9,25 +9,27 @@ def main():
             print("Иницирован выход из программы")
             break
         if num == 1:
-            name = input_name()
-            write_name(name)
-            print("Успешно записано\n")
+            show_telephones()
+            print("-- Все записи выведены --\n")
         if num == 2:
             a = input_found()
             a.rstrip()
             print(read_found(a))
         if num == 3:
+            name = input_name()
+            write_name(name)
+            print("Успешно записано\n")
+        if num == 4:
             sort()
             print("Успешно отсортировано\n")
-        if num == 4:
+        if num == 5:
+            name = input_change()
+            (changed, original) = change_name(name)
+            print(f"Успешно изменена запись: с\n {original}\n на {changed}")
+        if num == 6:
             name = input_delete()
             deleted = delete_name(name)
             print(f"Успешно удалена запись: {deleted}\n")
-        if num == 5:
-            show_telephones()
-            print("-- Все записи выведены --\n")
+
 
 main()
-
-
-
--- course/Seminar8/telephone/database.py
+++ course/Seminar8/telephone/database.py
@@ -1,4 +1,23 @@
-def write_name(name):
+def show_telephones():  # CASE 1
+    with open("tel.txt", "r") as file:
+        lst = file.readlines()
+        lst = [line.rstrip() for line in lst]
+        new_lst = list(enumerate(lst))
+    print("Телефонная книга:")
+    for i in range(len(new_lst)):
+        print(f"{new_lst[i][0]} --> {new_lst[i][1]}")
+
+
+def read_found(a):  # CASE 2
+    with open("tel.txt", "r") as file:
+        lst = file.readlines()
+        lst = [line.rstrip() for line in lst]
+        for i in lst:
+            if a.lower() in i.lower():
+                print(i)
+
+
+def write_name(name):  # CASE 3
     with open("tel.txt", "r") as file:
         lst = file.readlines()
     with open("tel.txt", "a") as file:
@@ -8,15 +27,7 @@ def write_name(name):
             file.write(f"\n{name}")
 
 
-def read_found(a):
-    with open("tel.txt", "r") as file:
-        lst = file.readlines()
-        for i in lst:
-            if a.lower() in i.lower():
-                print(i)
-
-
-def sort():
+def sort():  # CASE 4
     with open("tel.txt", "r") as file:
         lst = file.readlines()
         lst = [line.rstrip() for line in lst]
@@ -28,7 +39,37 @@ def sort():
             write_name(i)
 
 
-def delete_name(name):
+def change_name(name):  # CASE 5
+    with open("tel.txt", "r") as file:
+        lst = file.readlines()
+        lst = [line.rstrip() for line in lst]
+        new_lst = list(enumerate(lst))
+        for i in range(len(new_lst)):
+            if name.lower() in new_lst[i][1].lower():
+                print(f"{new_lst[i][0] + 1} --> {new_lst[i][1]}")
+        input_text = "Для изменения введите номер записи для изменения\nДля отмены введите 0\n"
+        del_line = int(input(input_text))
+        if del_line == 0:
+            new_line = old_line = "Изменение отменено"
+        else:
+            new_line = input(f"Введите новое значение для записи {del_line}:\n")
+            with open('tel.txt', 'w') as file:
+                for i in range(len(new_lst)):
+                    if del_line - 1 != new_lst[i][0]:
+                        if i == 0:
+                            file.write(new_lst[i][1])
+                        else:
+                            file.write(f"\n{new_lst[i][1]}")
+                    else:
+                        old_line = new_lst[i][1]
+                        if i == 0:
+                            file.write(new_line)
+                        else:
+                            file.write(f"\n{new_line}")
+    return (new_line, old_line)
+
+
+def delete_name(name):  # CASE 6
     with open("tel.txt", "r") as file:
         lst = file.readlines()
         lst = [line.rstrip() for line in lst]
@@ -39,7 +80,7 @@ def delete_name(name):
         input_text = "Для удаления введите номер записи для удаления\nДля отмены введите 0\n"
         del_line = int(input(input_text))
         if del_line == 0:
-            print("Удаление отменено")
+            deleted = "Удаление отменено"
         else:
             with open('tel.txt', 'w') as file:
                 for i in range(len(new_lst)):
@@ -51,13 +92,3 @@ def delete_name(name):
                     else:
                         deleted = new_lst[i][1]
     return deleted
-
-
-def show_telephones():
-    with open("tel.txt", "r") as file:
-        lst = file.readlines()
-        lst = [line.rstrip() for line in lst]
-        new_lst = list(enumerate(lst))
-    print("Телефонная книга:")
-    for i in range(len(new_lst)):
-        print(f"{new_lst[i][0]} --> {new_lst[i][1]}")
